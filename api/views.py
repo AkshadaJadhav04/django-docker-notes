d4a0618a512c9b0309dab1d4c847e3b7895a38c8
Akshada Jadhav <akshadajadhav0405@gmail.com>
2026-03-06 00:34:18 +0530

    Initial Django Docker Project

---
+++ api/views.py
@@ -0,0 +1,69 @@
+# from django.shortcuts import render
+#
+# # Create your views here.
+# #
+# # from django.http import JsonResponse
+# #
+# #
+# # def hello(request):
+# #     return JsonResponse({"message": "Hello Akshada"})
+# from django.http import JsonResponse
+# from .models import Note
+#
+#
+# def get_notes(request):
+#     notes = Note.objects.all()
+#
+#     data = []
+#     for note in notes:
+#         data.append({
+#             "id": note.id,
+#             "title": note.title,
+#             "content": note.content
+#         })
+#
+#     return JsonResponse(data, safe=False)
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Note
+from .serializers import NoteSerializer
+
+
+@api_view(['GET', 'POST'])
+def notes_list(request):
+    if request.method == 'GET':
+        notes = Note.objects.all()
+        serializer = NoteSerializer(notes, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = NoteSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def note_detail(request, pk):
+    try:
+        note = Note.objects.get(pk=pk)
+    except Note.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = NoteSerializer(note)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = NoteSerializer(note, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        note.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
